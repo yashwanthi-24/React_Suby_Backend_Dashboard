6c1c354f4be88febd1d901c6afc02ad05e627789
ashishchillale556 <ashishchillale556@gmail.com>
2025-08-21 15:23:38 +0530

    changed path to live in api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000"
+// export const API_URL = "http://localhost:4000"
 
-// export const API_URL = "https://backend-nodejs-suby-eg2a.onrender.com"
+export const API_URL = "https://backend-nodejs-suby-eg2a.onrender.com"
